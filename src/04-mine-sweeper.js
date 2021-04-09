b84f81d8b7179ffcb73fbf61a3c2b7e927bc9ee6
alexandex <kienva1992@gmail.com>
2021-04-09 18:35:24 +0300

    feat: 4.5 questions complated

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,18 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+
+  let pairs = [];
+  for(let i = 0; i < matrix.length; i++){
+    for(let j = 0; j < matrix[i].length; j++){
+      if(matrix[i][j] === true){
+        pairs.push([i, j]);
+      }
+    }
+    
+  }
+  return pairs;
 }
 
 module.exports = minesweeper;
--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -1,3 +1,5 @@
+const { match } = require("sinon");
+
 /**
  * Given some integer, find the maximal number you can obtain
  * by deleting exactly one digit of the given number.
@@ -9,8 +11,28 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let result = 0;
+  let numDigit = [];
+
+  while(n){
+    numDigit.push(num % 10);
+    num = Math.floor(num / 10);
+  }
+
+  for(let i = 0; i < numDigit.length; i++){
+
+    let num = 0;
+    for(let j = numDigit.length - 1; j >= 0; j-- ){
+      if(i !== i){
+        num = num * 10 + numDigit[j];
+      }
+
+    }
+    result = Math.max(num, result);
+  }
+return result;
+
 }
 
 module.exports = deleteDigit;
